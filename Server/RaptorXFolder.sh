c4cab10ae830bb396548eaf2d585d59f06a14633
Jinbo Xu <jinboxu@gmail.com>
2020-09-25 10:55:58 -0500

    minor

--- Server/RaptorXFolder.sh
+++ Server/RaptorXFolder.sh
@@ -7,15 +7,15 @@ cmdDir=`dirname $cmd`
 export ModelingHome=`dirname $cmdDir`
 #****************************************************************************************************************************************************************************
 
+. $ModelingHome/raptorx-path.sh
 . $ModelingHome/raptorx-external.sh
 
-export DistFeatureHome=$ModelingHome/BuildFeatures/
-export DL4DistancePredHome=$ModelingHome/DL4DistancePrediction4/
-export DL4PropertyPredHome=$ModelingHome/DL4PropertyPrediction/
-export DistanceFoldingHome=$ModelingHome/Folding/
-
-export PYTHONPATH=$ModelingHome:$PYTHONPATH
-export PATH=$ModelingHome/bin:$PATH
+#export DistFeatureHome=$ModelingHome/BuildFeatures/
+#export DL4DistancePredHome=$ModelingHome/DL4DistancePrediction4/
+#export DL4PropertyPredHome=$ModelingHome/DL4PropertyPrediction/
+#export DistanceFoldingHome=$ModelingHome/Folding/
+#export PYTHONPATH=$ModelingHome:$PYTHONPATH
+#export PATH=$ModelingHome/bin:$PATH
 
 outDir=`pwd`
 GPU=-1
